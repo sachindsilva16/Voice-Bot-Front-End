28d0669b9c239892d63c2939dda641dab47cb182
sachindsilva16 <sachindsilvad@gmail.com>
2023-04-23 10:44:51 +0530

    Added Component Files

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,11 +2,36 @@ import React from "react";
 
 function App(){
     return (
-        <div className="flex flex-row justify-between items-center md:space-x-10">
-            <li>Items</li>
-            <li>Items</li>
-            <li>Items</li>
+
+        // ----- Main Container -----
+
+        <div className="container max-w-6xl mx-auto px-6 py-8">
+
+
+            {/* Menu / Logo Container  */}
+
+
+            {/* Logo  */}
+
+
+            {/* Title of the Container  */}
+
+
+            {/* Menu Container  */}
+
+            {/* Three Dot Container  */}
+
+            {/* Drop-Down Arrow Container  */}
+
         </div>
+
+
+
+
+
+        // <div className="flex flex-row justify-between items-center md:space-x-10 px-6 py-8">
+            
+        // </div>
     );
 }
 
